aaa768157d3d30ebeab59ed2dc5ab98532cbb64a
Egliz <egliz3010@gmail.com>
2026-09-06 13:26:06 +0200

    feat: add NoGenericMethods class

---
+++ src/main/java/Level_1/Ex1_NoGenericMethods/NoGenericMethods.java
@@ -0,0 +1,35 @@
+package Level_1.Ex1_NoGenericMethods;
+
+public class NoGenericMethods {
+
+    private String element1;
+    private String element2;
+    private String element3;
+
+    public NoGenericMethods(String element2, String element3, String element1) {
+        this.element2 = element2;
+        this.element3 = element3;
+        this.element1 = element1;
+    }
+
+    public String getElement2() {
+        return element2;
+    }
+
+    public String getElement3() {
+        return element3;
+    }
+
+    public String getElement1() {
+        return element1;
+    }
+
+    @Override
+    public String toString() {
+        return "NoGenericMethods{" +
+                "element1='" + element1 + '\'' +
+                ", element2='" + element2 + '\'' +
+                ", element3='" + element3 + '\'' +
+                '}';
+    }
+}
