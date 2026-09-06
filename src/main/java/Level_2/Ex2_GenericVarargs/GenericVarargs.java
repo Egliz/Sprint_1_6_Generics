58c8678c28774f4820380b2baecbdaf3c30bc664
Egliz <egliz3010@gmail.com>
2026-09-06 16:50:17 +0200

    feat: add the printing of the fixed argument

--- src/main/java/Level_2/Ex2_GenericVarargs/GenericVarargs.java
+++ src/main/java/Level_2/Ex2_GenericVarargs/GenericVarargs.java
@@ -2,7 +2,10 @@ package Level_2.Ex2_GenericVarargs;
 
 public class GenericVarargs {
 
-    public static <T> void printAll(T... elements) {
+    public static <T> void printAll(String fixedElement, T... elements) {
+
+        System.out.println("Fixed element: " + fixedElement);
+
         for (T element : elements) {
             System.out.println(element);
         }
