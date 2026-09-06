f15f99a9c960556b09f5a2bc3b99794840bc54cf
Egliz <egliz3010@gmail.com>
2026-09-06 14:25:19 +0200

    feat: add GenericMethods class and printElements method

--- src/main/java/Level_1/Ex2_GenericMethods/GenericMethods.java
+++ src/main/java/Level_1/Ex2_GenericMethods/GenericMethods.java
@@ -1,4 +1,9 @@
 package Level_1.Ex2_GenericMethods;
 
 public class GenericMethods {
+
+    public static <T, U, V> void printElements(T element1, U element2, V element3) {
+
+        System.out.println("Element 1:" + element1 + "Element 2: " + element2 + "Element 3: " + element3);
+    }
 }
